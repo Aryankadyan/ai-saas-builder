ced03137c7e151dac816d1287b948395c75072dc
jai-max <kadyanaryan744@gmail.com>
2025-02-17 12:03:14 +0530

    Champ section

--- src/components/Champ.jsx
+++ src/components/Champ.jsx
@@ -5,10 +5,24 @@ import {logo} from '../assets'
 const Champ = () => {
   return (
     <header className='w-full flex justify-center items-center flex-col'>
-        <nav className='flex justify-between items-center flex-col'>
+        <nav className='flex justify-between items-center w-full mb-11 pt-4'>
             <img src={logo} alt='sumz_logo'
             className='w-30 object-contain'/>
+
+            <button 
+            type ='button'
+            onClick={()=> window.open('https://github.com/Aryankadyan/ai-saas-builder.git/')}
+            className='black_btn'>
+              Github
+            </button>
         </nav>
+        <h1 className='head_text'>Summarize Articles with
+          <br className='max-md:hidden'/>
+          <span className="orange_gradient">Artifical Intelligence</span>
+        </h1>
+        <h2 className='desc'>
+        "Summarize Articles Instantly with Cutting-Edge AI for Clear, Concise Insights" 🚀
+        </h2>
     </header>
 
     
