3332443095f9ec39b0d73e97ea691b4a1c0a3e01
jai-max <kadyanaryan744@gmail.com>
2025-02-17 11:37:20 +0530

    Setup done

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import Demo from './components/Demo'
+import Champ from './components/Champ'
+import './App.css'
+
+const App = () => {
+  return (
+    <main>
+        <div className='main'>
+            <div className='gradient'/>
+        </div>
+
+<div className='app'>
+    <Demo/>
+    <Champ/>
+</div>
+
+    </main>
+  )
+}
+
+export default App
